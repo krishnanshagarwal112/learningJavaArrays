04cee6f1edf61e036ad80aaccf19a47f9a0439e1
krishnanshagarwal112 <gonkillua2680@gmail.com>
2022-06-19 14:19:04 +0530

    started

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Syntax of array
+        // datatype[] ref variable = new datatype[size] // datatype represents the type of data in the array.
+        // we can have only one type of data in array
+        // Store 5 numbers
+        int[] rollns  = new int[5]; // rollns is reference varibale, right side is the object
+        // or directly
+        int[] rollns2 = {23,2,3,2,4,1};
+
+        int ros[];// declaration of array. ros is getting defined in stack memory
+        ros = new int[5]; // initialization of array. ros is getting . object is created in heap memory
+
+        String[] arr = new String[4];   // String array
+
+    }
+}
